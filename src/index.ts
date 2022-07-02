389b7a814cf2199f90f87fc571e5d33a7b13dc32
Hackermon <git@daniel.is-a.dev>
2022-07-02 12:01:09 -0400

    pub

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+// eslint-disable-next-line import/order
+import path from 'path';
+import dotenv from 'dotenv';
+
+dotenv.config({ 'path': path.join(__dirname, '../../.env') });
+
+import TwoBTwo from "./lib/core/2b2t";
+
+const t = new TwoBTwo({
+    'username': process.env['MINECRAFT_USERNAME'],
+    'password': process.env['MINECRAFT_PASSWORD'],
+    'auth': process.env['MINEFLAYER_AUTH'] == 'mojang' ? 'mojang' : 'microsoft',
+});
+
+t.start();
---
+++ src/types/mineflayer.d.ts
@@ -0,0 +1,10 @@
+import mineflayer from 'mineflayer';
+
+
+declare module 'mineflayer' {
+    // eslint-disable-next-line functional/prefer-type-literal
+    interface Bot extends Mineflayer.Bot {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        readonly afk: any;
+    }
+}
