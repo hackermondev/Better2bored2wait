28f2461a7401f242d11c9319958249ef2d44733a
Hackermon <git@daniel.is-a.dev>
2022-07-03 13:54:00 -0400

    fix(app): redo logging framework

--- src/lib/core/logger.ts
+++ src/lib/core/logger.ts
@@ -1,9 +1,21 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 /* eslint-disable functional/no-class */
-import { LogLevel } from '@sapphire/framework';
-import { Logger } from '@sapphire/plugin-logger';
+import path from 'path';
+
+import Logger from '@ptkdev/logger';
+
+const logsFolder = path.join(process.cwd(), '2bored2wait-logs');
 
 export class LoggerExtended extends Logger {
   constructor() {
-    super({ level: LogLevel.Debug });
+    super({ 
+      'debug': process.env['DEBUG'] != undefined,
+      'path': {
+        //@ts-ignore
+        'debug_log': process.pkg ? path.join(logsFolder, 'debug.log') : undefined,
+        //@ts-ignore
+        'error_log': process.pkg ? path.join(logsFolder, 'error.log') : undefined
+      }
+    });
   }
 }
